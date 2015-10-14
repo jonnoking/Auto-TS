214af872796f4a3169b7ca2d39805d0dcf91fc7c
jonnoking <jonno@jonnoking.com>
2015-10-14 04:21:45 -0700

    deploy from here script

---
+++ DeployK2PackagesFoundHere.ps1
@@ -0,0 +1,74 @@
+﻿# Disable Execution Policy
+Set-ExecutionPolicy Unrestricted
+
+$Continue = Read-Host -Prompt "This script will deploy all K2 Packages (*.KSPX) in this directory. It may overwrite existing assets. Please use the K2 Package and Deployment tool if you are unsure. Do you wish to continue? (Yes [Y] | No [N]) "
+if ($Continue -ne "Y" -and $Continue -ne "Yes")
+{
+    Write-Host "The script has stopped at your request."
+    return
+}
+
+
+
+set-alias installutil $env:windir\Microsoft.NET\Framework64\v4.0.30319\installutil 
+installutil -u /AssemblyName 'SourceCode.Deployment.PowerShell, Version=4.0.0.0, Culture=neutral, PublicKeyToken=16a2c5aaaa1b130d, processorArchitecture=MSIL' 
+installutil -i /AssemblyName 'SourceCode.Deployment.PowerShell, Version=4.0.0.0, Culture=neutral, PublicKeyToken=16a2c5aaaa1b130d, processorArchitecture=MSIL'
+Add-PSSnapin SourceCode.Deployment.PowerShell
+return
+
+#Get K2 Connection String
+$K2ConnectionString = Get-K2ConnectionString
+
+$ScriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+
+Get-ChildItem -Path $ScriptPath"\*" -Include *.kspx | 
+Foreach-Object{
+    #foreach KSPX found deploy package   
+    Deploy-Package ($_.FullName) -ConnectionString $K2ConnectionString.ConnectionString -NoAnalyze -ErrorAction Inquire    
+
+}
+
+$K2ConnectionString = $null
+
+
+function Get-K2ConnectionString {
+<#
+	.Synopsis
+		This function returns a connection string to connect to K2
+  .Description
+		This function returns a connection string to connect to K2    
+  .Example
+		Get-K2ConnectionString "localhost"
+  .Example
+		Get-K2ConnectionString "dlx" $true
+	.Parameter $server
+		The K2 server to connect to. Defaults to localhost
+	.Parameter $port
+		The port to use. Defaults to 5555
+	.Notes
+		AUTHOR: Paul Kelly, K2
+		#Requires -Version 2.0
+#>
+	[CmdletBinding()]
+	Param(		
+		[ValidateNotNullOrEmpty()]
+		[string]$server="localhost",
+		[ValidateNotNullOrEmpty()]
+		[int]$port=5555
+	)
+	BEGIN
+	{
+		[System.Reflection.Assembly]::LoadWithPartialName("SourceCode.HostClientAPI") | Out-Null
+	}
+	PROCESS
+	{
+		$conn = New-Object SourceCode.Hosting.Client.BaseAPI.SCConnectionStringBuilder 
+		$conn.IsPrimaryLogin = $true
+		$conn.Integrated = $true
+		$conn.Host = $server
+		$conn.Port = $port
+		Write-Output $conn
+	}
+}
+ 
